b703ce0f145ad4c2acf0a6b63c7fe75ac9fa9d21
Emna Bahri <97821331+baemmma@users.noreply.github.com>
2023-06-06 16:11:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="HugChat - An LLM-powered Streamlit app")
 
 # Sidebar contents
 with st.sidebar:
-    st.title('🤗💬 HugChat App')
+    st.title('🤗💬 VivaChat App')
     st.markdown('''
     ## About
     This app is an LLM-powered chatbot built using:
@@ -34,31 +34,56 @@ input_container = st.container()
 colored_header(label='', description='', color_name='blue-30')
 response_container = st.container()
 
-# User input
-## Function for taking user provided prompt as input
-def get_text():
-    input_text = st.text_input("You: ", "", key="input")
-    return input_text
-## Applying the user input box
-with input_container:
-    user_input = get_text()
 
-# Response output
-## Function for taking user prompt as input followed by producing AI generated responses
-def generate_response(prompt):
-    chatbot = hugchat.ChatBot()
-    response = chatbot.chat(prompt)
-    return response
+with open("chatbot_database.txt", "r") as file:
+    chatbot_db = file.read()
+    
+sentences = nltk.sent_tokenize(chatbot_db) 
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer()
 
-## Conditional display of AI generated responses as a function of user provided prompts
-with response_container:
-    if user_input:
-        response = generate_response(user_input)
-        st.session_state.past.append(user_input)
-        st.session_state.generated.append(response)
+# Compute the TF-IDF
+tf_idf = vectorizer.fit_transform(sentences).toarray()
+
+# Create a DataFrame with the TF-IDF representation
+tf_idf_df = pd.DataFrame(data=tf_idf, columns=vectorizer.get_feature_names_out())
+
+greetings_inputs = ['Hello', 'Hey', 'Hi', 'Good morning', 'Good evenig', 'What\'s up?']
+greetings_outputs = ['Hi, I am Vivabot. How can I assist you?', 'Hello! How may I help you?', 'Hey there! How can I assist you today?', 'Hey! What can I do for you?']
+
+
+def greetings(sentence, greetings_inputs, greetings_outputs):
+    if sentence.lower() in [x.lower() for x in greetings_inputs]:
+        output = random.choice(greetings_outputs)
+        return output
+    else:
+        return None
+
+def get_closest_sentence(query, tf_idf, vectorizer, database):
+    query_tf_idf = vectorizer.transform([query])
+    similarity = cosine_similarity(query_tf_idf, tf_idf)
+    closest_sentence_index = np.argmax(similarity)
+    return database[closest_sentence_index]
+
+def vivabot(greetings_inputs, greetings_outputs, tf_idf, vectorizer, database):
+    print("Welcome to Vivabot!")
+    print("How can I assist you today?")
+    
+    while True:
+        user_input = input("User input: ")
         
-    if st.session_state['generated']:
-        for i in range(len(st.session_state['generated'])):
-            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-            message(st.session_state["generated"][i], key=str(i))
-Conclusion
+        if user_input.lower() == "bye":
+            print("Goodbye! Have a great day!")
+            break
+        
+        greetings_output = greetings(user_input, greetings_inputs, greetings_outputs)
+        
+        if greetings_output:
+            print(greetings_output)
+        else:
+            closest_sentence = get_closest_sentence(user_input, tf_idf, vectorizer, database)
+            print(closest_sentence)
+            
+            
+vivabot(greetings_inputs, greetings_outputs, tf_idf, vectorizer, chatbot_db)
+
