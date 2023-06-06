f8ccc4a57b59bb713604be1d01f8b3b6bd03eb3d
Emna Bahri <97821331+baemmma@users.noreply.github.com>
2023-06-06 15:53:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,10 @@ from streamlit_extras.colored_header import colored_header
 from streamlit_extras.add_vertical_space import add_vertical_space
 from hugchat import hugchat
 
-st.set_page_config(page_title="HugChat - An LLM-powered Streamlit app")
+st.set_page_config(page_title="Vivabot - An LLM-powered Streamlit app")
+
+with open("chatbot_database.txt", "r") as file:
+    chatbot_db = file.read()
 
 # Sidebar contents
 with st.sidebar:
